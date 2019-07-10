20c99b31d8679e6e24dda7660e7156d502d37a1d
paul42 <Paul.42@gmail.com>
2019-07-10 18:34:04 +0000

    update config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_2uV7Vtt3d', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1e1osso6i6v85j6oq7kh88t5ki', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
