3a17ad0d42e6b0a83e9285f5a76177f1e28552e5
paul42 <Paul.42@gmail.com>
2019-07-10 19:08:35 +0000

    adding invoke url

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_2uV7Vtt3d', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1e1osso6i6v85j6oq7kh88t5ki', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://jl26xk7hc6.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
